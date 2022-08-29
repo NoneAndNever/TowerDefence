901e4a096b2b31ad6f03fcd566c87b86c5ebacd0
NoneAndNever <43804135+NoneAndNever@users.noreply.github.com>
2022-08-29 22:09:07 +0800

    1

---
+++ Assets/Scripts/Tower/TowerArrowBase.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerArrowBase : TowerBase
+{
+    public static float universalDamageMultiplier = 1;
+    private float damageMultiplier;
+    private float AtkTimer;
+
+
+    private void Update()
+    {
+        //Todo:攻击计时器
+    }
+
+    protected override void Atk(GameObject Enemy)
+    {
+        //Todo：攻击行为
+    }
+}
+
+
+    
+    
--- Assets/Scripts/Tower/TowerBase.cs
+++ Assets/Scripts/Tower/TowerBase.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class TowerBase : MonoBehaviour
 {
     [Header("基本属性")]
-    protected int atkDamage;                 //攻击伤害
+    protected int atkDamageMin;              //攻击伤害(最小)
+    protected int atkDamageMax;              //攻击伤害(最大)
     protected float atkSpeed;                //攻击速度 
     protected float atkRadius;               //攻击半径
     protected EDamageType damageType;        //伤害类型(物理/魔法/真实)
@@ -13,5 +14,15 @@ public class TowerBase : MonoBehaviour
     [Header("组件")]
     protected Rigidbody _rigidbody;
     protected Collider2D col;
+
+    protected virtual void Atk(GameObject Enemy)
+    {
+        ;
+    }
+    protected GameObject ChooseEnemy()
+    {
+     //Todo:检测范围内离目标点最近的敌人并返回   
+     return null;
+    }
 
 }
